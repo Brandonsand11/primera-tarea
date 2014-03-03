bd3fe3dbf61b28e85226b04c38e28ca07184505f
Brandon sandoval <brandon.sand15@gmail.com>
2014-03-03 12:01:02 -0500

    primera tarea
    esta es mi primera tarea

---
+++ primera tarea de brandon.py
@@ -0,0 +1,60 @@
+﻿print("hola mundo cruel")
+
+print("******************soy goku*******************")
+
+print("te damos la bienvenida")
+
+#Voy a empezar a programar "brandon" :D
+
+brandon= 1
+while brandon <=100:
+    print("la disciplina vencerala inteligencia")
+    brandon+=1
+    
+j=1
+for jr in range(1,202):
+    print("Dios nunca cambia")
+    j+=1
+j=1,2,3,4,5,6,7,8,9,
+for j in range(1,10):
+    print(4,6,8,2,0)
+    j+=1
+
+
+resp = ("como te llamás: ")
+print ("Hola mucho gusto de conocerte",resp )
+nombre=input();
+print("mucho gusto: "+nombre)
+
+
+jo=1
+for jl in range(1,2):
+    print("dime un numero: ",input())
+    jo+=1
+
+
+
+
+
+num=input("dime otro numero: ")
+
+
+math=3.1416
+
+
+
+base = float(input("Ingrese la base del rectángulo,circulo,rombide,cuadrado,trapecio: "))
+altura = float(input("Ingrese la altura del rectángulo,circulo,rombide,cuadrado,trapecio: "))
+radio = float(input("Ingrese el radio del círculo: "))
+print("el area del ractangulo es: ", base * altura)
+print("el area del circulo es: ", math * radio ** 2)
+print("area romboide es: ",base*altura)
+print("el area del cuadrado es: ",base*base)
+print("el area de trapecio es: ",base+base*altura/2)
+
+velocidad = float(input("igrese la velocidad: "))
+espacio = float(input("ingrese el espacio: "))
+tiempo = float(input("ingrese el tiempo: "))
+print("la velocidad es: ", espacio/tiempo)
+print("el espacio es : ",velocidad*tiempo)
+print("el tiempo es : ",espacio/velocidad)
